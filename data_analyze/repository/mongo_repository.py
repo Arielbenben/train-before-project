cce0802b46fb220aca8e9cae8445439346680d3a
Ariel <relbh18@gmail.com>
2024-12-17 11:46:33 +0200

    MS. elkana     hhhh

--- data_analyze/repository/mongo_repository.py
+++ data_analyze/repository/mongo_repository.py
@@ -10,6 +10,7 @@ def get_average_of_gpa():
 
     return df['GPA'].mean()
 
+
 def get_average_of_sleep_hours_per_day():
     life_style = list(student_collection.find({}, {'life_style': 1, '_id': 0}))
     df = pd.DataFrame(map(lambda x: x['life_style'], life_style))
@@ -17,3 +18,20 @@ def get_average_of_sleep_hours_per_day():
 
     return df['Sleep_Hours_Per_Day'].mean()
 
+
+def get_students_exercise_1():
+    gpa_average = get_average_of_gpa()
+    print(gpa_average)
+    sleep_hour_average = get_average_of_sleep_hours_per_day()
+    students = student_collection.find(
+        {
+            '$and': [
+                {int('life_style.GPA'): {'$gt': gpa_average}},
+                {int('life_style.Sleep_Hours_Per_Day'): {'$lt': sleep_hour_average}}
+            ]
+        })
+
+    return list(students)
+
+
+print(get_students_exercise_1())
--- mongo_consumer_app/repository/students_repository.py
+++ mongo_consumer_app/repository/students_repository.py
@@ -11,18 +11,42 @@ def remove_field_student_id(dic: dict):
     return dic
 
 
+def convert_life_style_to_float(life_style: dict):
+    fields_to_convert = [ 'GPA', 'Sleep_Hours_Per_Day', 'Study_Hours_Per_Day',
+        'Extracurricular_Hours_Per_Day', 'Social_Hours_Per_Day', 'Physical_Activity_Hours_Per_Day']
+
+    for field in fields_to_convert:
+        life_style[field] = float(life_style.get(field, 0))
+
+    return life_style
+
+
 def insert_life_style(life_styles: list):
     for life_style in life_styles:
+        convert_life = convert_life_style_to_float(life_style)
         student_collection.update_one(
-            {'id': life_style['student_id']},
-            {'$set': {'life_style': remove_field_student_id(life_style)}}
+            {'id': convert_life['student_id']},
+            {'$set': {'life_style': remove_field_student_id(convert_life)}}
         )
+
+
+def convert_performance_to_float(performance: dict):
+    fields_to_convert = ['current_grade', 'attendance_rate', 'assignments_completed', 'missed_deadlines',
+            'participation_score', 'midterm_grade', 'study_group_attendance', 'office_hours_visits',
+            'extra_credit_completed']
+
+    for field in fields_to_convert:
+        performance[field] = float(performance.get(field, 0))
+
+    return performance
+
 
 def insert_performance(performances: list):
     for performance in performances:
+        convert_performance = convert_performance_to_float(performance)
         student_collection.update_one(
-            {'id': performance['student_id']},
-            {'$set': {'performance': remove_field_student_id(performance)}}
+            {'id': convert_performance['student_id']},
+            {'$set': {'performance': remove_field_student_id(convert_performance)}}
         )
 
 
